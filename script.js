d7c69f94bc98066f16da31dc2acdb6a9333fb82e
Lucas Misael <luckmca@gmail.com>
2020-12-29 11:43:01 -0300

    setando o loop

--- script.js
+++ script.js
@@ -32,7 +32,21 @@ function criarCobrinha(){
 
 }
 
+document.addEventListener("keydown", update);     //37, 38, 39, 40  =  codigos do teclado para direnta, baixo, esquerda, cima.  EventListener = escuta o que o usuario apertar
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+} 
+
+// fazendo o loop da cobrinha com o if
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     
     criarBG(); 
     criarCobrinha();
