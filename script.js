ce60598f75f74182c2ac32f8a9c73957d3522e9b
Lucas Misael <luckmca@gmail.com>
2020-12-29 13:16:00 -0300

    finalizado o js

--- script.js
+++ script.js
@@ -61,6 +61,12 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Fim de jogo! Pontuação: '+ snake.length * 10); //conta para mostrar a pontuação.
+        }
+    }
     
     criarBG(); 
     criarCobrinha();
